671bc10bbd5516884e1e269563ed00148a7f8195
Russell Morrisey <rmorrise@users.noreply.github.com>
2018-02-11 02:14:00 -0500

    Made index page data-driven.

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PlaybookFile {
+  name: string
+  file: string
+}
+
 @Component({
   selector: 'st-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  files: PlaybookFile[] = [
+    {name: 'The Captain', file: 'captain'},
+    {name: 'The XO', file: 'xo'},
+    {name: 'The Chief Engineer', file: 'engineer'},
+    // <!--<a href="playbooks/tactical"><li>Tactical Officer</li></a>-->
+    // <!--<a href="playbooks/medic"><li>The Chief Medic</li></a>-->
+    // <!--<a href="playbooks/operations"><li>Operations Officer</li></a>-->
+    // <!--<a href="playbooks/science"><li>Science Officer</li></a>-->
+  ]
 
   constructor() { }
 
