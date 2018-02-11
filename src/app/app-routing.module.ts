918409a7fd0a34d41f08bad579fb6b9352485848
Russell Morrisey <rmorrise@users.noreply.github.com>
2018-02-11 11:28:54 -0500

    Fixed redirect to index.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {environment} from '../environments/environment';
 import {IndexComponent} from './index/index.component';
 
 const routes: Route[] = [
-  {path: '', redirectTo: 'playbooks/captain', pathMatch: 'full'},
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
   {path: 'index', component: IndexComponent},
   {path: 'playbooks/:file', component: PlaybookComponent}
 ];
