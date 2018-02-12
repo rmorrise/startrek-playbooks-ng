232d67c76123c80d139212b22feef90ff683821f
Russell Morrisey <rmorrise@users.noreply.github.com>
2018-02-11 21:53:33 -0500

    Added captain's descriptors.

--- src/app/playbook/model/playbookData.ts
+++ src/app/playbook/model/playbookData.ts
@@ -1,10 +1,12 @@
 import {Move} from './move';
+import {StatLineDescriptor} from './statLineDescriptor';
 
 export interface PlaybookData {
   playbookName: string
   requiredMoves?: Move[]
   numOptionalMoves: number
   optionalMoves: Move[]
+  descriptors: StatLineDescriptor[]
   roleFluff: string
   quote: string
 }
---
+++ src/app/playbook/model/statLineDescriptor.ts
@@ -0,0 +1,11 @@
+export interface StatLineDescriptor {
+  name: string
+  stats: {
+    war: number
+    peace: number
+    duty: number
+    explore: number
+    rank: number
+  }
+  advancement: string
+}
